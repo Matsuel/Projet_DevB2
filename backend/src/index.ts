5e31dd74b380d189cadb402167f7d6f52da5b928
Matsuel <matsuel0308@gmail.com>
2024-03-09 17:02:27 +0100

    add channels and functions to do

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -45,3 +45,31 @@ connectToMongo();
 server.listen(3500, () => {
     console.log('Server is running on port 3500');
 });
+
+
+//Canaux de communication websockets
+
+// login/register
+// getConversations
+// getMessages permet de récupérer les messages d'une conversation
+// sendMessage permet d'envoyer un message dans une conversation
+// createConversation permet de créer une conversation entre deux utilisateurs
+// deleteConversation permet de supprimer une conversation
+// getComments permet de récupérer les commentaires sur une auto école/moniteur
+// postComment permet de poster un commentaire sur une auto école/moniteur
+// deleteComment permet de supprimer un commentaire sur une auto école/moniteur
+// postReview permet de poster un avis sur une auto école/moniteur
+// deleteReview permet de supprimer un avis sur une auto école/moniteur
+// getProfile permet de récupérer le profil de l'utilisateur connecté, en fonction de son type (auto école/moniteur/élève)
+// updateProfile permet de mettre à jour le profil de l'utilisateur connecté, en fonction de son type (auto école/moniteur/élève)
+// getAutoEcole permet de récupérer les informations d'une auto école
+// search permet de rechercher une auto école ou une ville afin de récupérer les auto écoles correspondantes
+// getMoniteur permet de récupérer les commentaires et avis d'un moniteur
+// getAutoEcole permet de récupérer les commentaires et avis d'une auto école
+// synchroMessages permet de synchroniser les messages d'une conversation entre deux utilisateurs si le destinataire n'est pas connecté on envoie un mail pour le notifier s'il a activé l'option
+
+// Fonctions
+// sendMail permet d'envoyer un mail
+// synchroneMessages permet de synchroniser les messages d'une conversation entre deux utilisateurs
+
+// voir si les autos écoles en db ont des objets avec les avis et commentaires
