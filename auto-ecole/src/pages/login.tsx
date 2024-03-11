a90c85e1d0baec1c84808081e59681a711e90288
ninjawill543 <lucas.hanson@ynov.com>
2024-03-11 11:39:21 +0100

    starting on login and register pages

---
+++ auto-ecole/src/pages/login.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Login: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <main>
+        <h1>login</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Login;
---
+++ auto-ecole/src/pages/register.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Register: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Register</title>
+      </Head>
+      <main>
+        <h1>Register</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Register;
