81eb17ff22da9c5f64bc36dcaf36c1b7a9ad198a
ninjawill543 <lucas.hanson@ynov.com>
2024-03-12 10:00:57 +0100

    working on moniteur page

--- auto-ecole/src/pages/moniteur.tsx
+++ auto-ecole/src/pages/moniteur.tsx
@@ -10,6 +10,9 @@ const Moniteur: React.FC = () => {
     </Head>
     <main>
       <Header />
+      <h1>nom de moniteur</h1>
+      <h2>nom d'auto ecole, lien clickable</h2>
+      <h2>address auto ecole</h2>
     </main>
   </div>
   );
