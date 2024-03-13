4469e521c34161f393ca296772a58207c3f8a068
Matsuel <matsuel0308@gmail.com>
2024-03-13 16:38:29 +0100

    edit readme + edit AutoEcole model

--- backend/dist/MongoModels/Users.js
+++ backend/dist/MongoModels/Users.js
@@ -5,13 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.User = exports.Student = exports.AutoEcole = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
+const MonitorSchema = new mongoose_1.default.Schema({
+    name: { type: String, required: true },
+});
 const AutoEcole = mongoose_1.default.model("AutoEcole", new mongoose_1.default.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
     address: { type: String, required: true },
     pics: { type: [String], required: false },
-    monitors: { type: [String], required: true },
+    monitors: { type: [MonitorSchema], required: true },
     phone: { type: String, required: true },
     card: { type: Boolean, required: true },
     cheque: { type: Boolean, required: true },
@@ -27,6 +30,15 @@ const AutoEcole = mongoose_1.default.model("AutoEcole", new mongoose_1.default.S
     fin_francetravail: { type: Boolean, required: true },
     formations: { type: [String], required: true },
     students: { type: [String], required: true },
+    note: { type: Number, required: false },
+    noteCount: { type: Number, required: false },
+    reviews: {
+        type: [{
+                studentId: { type: String, required: true },
+                content: { type: String, required: true },
+                date: { type: Date, required: true },
+            }],
+    }
     // subscribeDate: { type: Date, required: true },
 }));
 exports.AutoEcole = AutoEcole;
