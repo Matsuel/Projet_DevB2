518096b22855fe798950b42df25dc937e8a7c63e
Matsuel <matsuel0308@gmail.com>
2024-03-26 12:26:54 +0100

    add todo

--- auto-ecole/src/Functions/Chat.ts
+++ auto-ecole/src/Functions/Chat.ts
@@ -4,6 +4,8 @@ const createConversation = async (userId : string, creatorId : string) => {
     const conversation = await axios.post("http://localhost:3500/createConversation", {userId, creatorId})
     console.log(conversation.data)
     return conversation.data
+    //voir pour rediriger vers la page de chat/id
+    //try catch si ça existe pas
 }
 
 export {createConversation}
