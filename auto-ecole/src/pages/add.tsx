7930f786adeae4fe10c1f8deda5082aa80bd2bb1
Matsuel <matsuel0308@gmail.com>
2024-05-07 11:03:04 +0200

    add style

--- auto-ecole/src/pages/add.tsx
+++ auto-ecole/src/pages/add.tsx
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Header from "@/Components/Header";
 import ReactStars from 'react-stars';
-import styles from '@/styles/add.module.css';
-import axios from 'axios';
+import styles from '@/styles/add.module.scss';
 import { useRouter } from 'next/router';
 import { ReviewMonitor } from '@/types/Monitor';
 import { getToken } from '@/Functions/Token';
@@ -24,10 +23,6 @@ const Add: React.FC = () => {
   const [monitorsReview, setMonitorsReview] = useState<ReviewMonitor[]>([]);
 
   let token = getToken(router, jwtDecode);
-
-  // useEffect(() => {    
-  //   token && token !== '' ? fetchData(setMonitorsReview, token as string, router) : router.push('/login');
-  // }, []);
 
   useEffect(() => {
     if (token && token !== '') {
@@ -61,7 +56,7 @@ const Add: React.FC = () => {
     } else {
       alert('Erreur lors de la publication de l\'avis');
     }
-  });       
+  });
 
   return (
     <div>
@@ -70,44 +65,47 @@ const Add: React.FC = () => {
       </Head>
       <main>
         <Header />
-        <form>
-          <h1>Super nom d'auto ecole</h1>
-          <ReactStars
-            count={5}
-            size={24}
-            color2={'#ffd700'}
-            value={autoecoleReview.stars}
-            onChange={(newRating) => setAutoecoleReview({ ...autoecoleReview, stars: newRating })}
-          />
-          <textarea id="autoecole-comment" placeholder='Commentaire' className={styles.add} required onChange={(e) => setAutoecoleReview({ ...autoecoleReview, comment: e.target.value })} />
-          <button type="button" onClick={() => handleSubmitAutoecole(router, autoecoleReview, token as string)}>avis autoecole</button>
+        <div className={styles.main}>
+          <form className={styles.form}>
+            <h1 className={styles.title}>Super nom d'auto ecole</h1>
+            <ReactStars
+              className={styles.stars}
+              count={5}
+              size={24}
+              color2={'#ffd700'}
+              value={autoecoleReview.stars}
+              onChange={(newRating) => setAutoecoleReview({ ...autoecoleReview, stars: newRating })}
+            />
+            <textarea id="autoecole-comment" placeholder='Commentaire' className={styles.inputText} required onChange={(e) => setAutoecoleReview({ ...autoecoleReview, comment: e.target.value })} />
+            <button className={styles.buttonadd} type="button" onClick={() => handleSubmitAutoecole(router, autoecoleReview, token as string)}>Poster l'avis sur l'auto ecole</button>
 
-          {monitorsReview.map((monitor, index) => {
-            return (
-              <div key={index}>
-                <h2>{monitor.name}</h2>
-                <ReactStars
-                  count={5}
-                  size={24}
-                  color2={'#ffd700'}
-                  value={monitor.stars}
-                  onChange={(newRating) => {
+            {monitorsReview.map((monitor, index) => {
+              return (
+                <div key={index} className={styles.form}>
+                  <h2 className={styles.title}>{monitor.name}</h2>
+                  <ReactStars
+                    className={styles.stars}
+                    count={5}
+                    size={24}
+                    color2={'#ffd700'}
+                    value={monitor.stars}
+                    onChange={(newRating) => {
+                      const newMonitorsReview = [...monitorsReview];
+                      newMonitorsReview[index].stars = newRating;
+                      setMonitorsReview(newMonitorsReview);
+                    }}
+                  />
+                  <textarea id="prof1-comment" placeholder='Commentaire' className={styles.inputText} required onChange={(e) => {
                     const newMonitorsReview = [...monitorsReview];
-                    newMonitorsReview[index].stars = newRating;
+                    newMonitorsReview[index].comment = e.target.value;
                     setMonitorsReview(newMonitorsReview);
-                  }}
-                />
-                <textarea id="prof1-comment" placeholder='Commentaire' className={styles.add} required onChange={(e) => {
-                  const newMonitorsReview = [...monitorsReview];
-                  newMonitorsReview[index].comment = e.target.value;
-                  setMonitorsReview(newMonitorsReview);
-                }} />
-                <button type="button" onClick={() => handleSubmitMonitor(monitor, token as string, router)}>
-                  avis prof1</button>
-              </div>
-            )
-          })}
-        </form>
+                  }} />
+                  <button type="button" onClick={() => handleSubmitMonitor(monitor, token as string, router)} className={styles.buttonadd}>Poster l'avis prof1</button>
+                </div>
+              )
+            })}
+          </form>
+        </div>
       </main>
     </div>
   );
